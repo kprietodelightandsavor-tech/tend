183c64c31a7c7caffc3a81ab5b31018a554beaed
kprietodelightandsavor-tech <kprieto.delightandsavor@gmail.com>
2026-03-18 17:00:32 -0500

    Update HabitScreen.jsx

--- src/screens/HabitScreen.jsx
+++ src/screens/HabitScreen.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { HABIT_PROMPTS, HABIT_KEYS } from "../data/seed";
-import { AttentionIcon, NarrationIcon, OutdoorIcon, StillnessIcon, OrderlyIcon } from "../components/HabitScreen";
+import { AttentionIcon, NarrationIcon, OutdoorIcon, StillnessIcon, OrderlyIcon } from "./HabitScreen";
 
 const HABIT_ICON_MAP = { attention: AttentionIcon, narration: NarrationIcon, outdoor: OutdoorIcon, stillness: StillnessIcon, orderly: OrderlyIcon };
 
