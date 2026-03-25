e0e484caed6af93e93f5ab1d85ef741b30e4face
kprietodelightandsavor-tech <kprieto.delightandsavor@gmail.com>
2026-03-25 09:46:11 -0500

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,13 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://dxsmywjiawgifeyfkdcj.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_e0D-G3CQEtdhqsg4K21yEQ_mgkqy3bj';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4c215d2ppYXdnaWZleWZrZGNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4Nzc1MTIsImV4cCI6MjA4OTQ1MzUxMn0.LVUXLkGokJ9k68k73Hr2kumsJ94GqNkMCC08Bce2Cdw';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: false,
-    storage: window.localStorage,
   }
 });
